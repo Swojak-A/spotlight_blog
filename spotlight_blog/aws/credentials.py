8b2ad4806e62948052beb4b07a17f462ba3995c2
kaoss <swojak.a@gmail.com>
2018-10-31 20:22:46 +0100

    add aws for static files

---
+++ spotlight_blog/aws/credentials.py
@@ -0,0 +1,2 @@
+access_key = "AKIAJYXTBX6PNYEGEDYQ"
+secret_key = "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"
--- spotlight_blog/settings/production.py
+++ spotlight_blog/settings/production.py
@@ -17,7 +17,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -29,6 +30,10 @@ SECRET_KEY=os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+# AWS S3 settings
+from spotlight_blog.aws.conf import *
+
 
 ALLOWED_HOSTS = ["spotlight-blog.herokuapp.com"]
 
